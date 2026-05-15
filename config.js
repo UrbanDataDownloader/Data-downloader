3748e09039041d75c51b1fd16521275193293d8b
Yourunwen <yourunwen1@gmail.com>
2026-05-15 14:51:57 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.__APP_CONFIG__ = {
-  API_BASE: "https://homeless-honolulu-looksmart-ton.trycloudflare.com",
+  API_BASE: "https://hypothesis-rights-controversial-maritime.trycloudflare.com",
 };
